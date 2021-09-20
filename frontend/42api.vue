ac1a71362694248dc8147a7172cd2fc5463fcc45
Tofwin Claudine <tclaudin@et-e5.kzn.21-school.ru>
2021-09-20 21:07:17 +0300

    api

---
+++ frontend/42api.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "googleAuth",
+  created() {
+    const requestOptions = {
+      method: "POST"
+    };
+    fetch("https://api.intra.42.fr/oauth/authorize?client_id=74bccccfc5c882fab475da83297ac3e5f3a0634bac58c1a51e1c10244be8ffd4&redirect_uri=https%3A%2F%2Fwww.google.com%2F&response_type=code", requestOptions)
+      .then(response => response.json())
+      .then(data => (this.postId = data.id));
+  }
+};
+</script>
---
+++ frontend/auth.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "googleAuth",
+  created() {
+    const requestOptions = {
+      method: "POST"
+    };
+    fetch("https://www.authenticatorApi.com/pair.aspx?AppName=ft_transcendence&SecretCode=1234567890", requestOptions)
+      .then(response => response.json())
+      .then(data => (this.postId = data.id));
+  }
+  connected() {
+	const requestOptions = {
+      method: "POST"
+    };
+	fetch("https://www.authenticatorApi.com/Validate.aspx?Pin=" + userPin + "&SecretCode=1234567890", requestOptions)
+      .then(response => response.json())
+      .then(data => (this.postId = data.id));
+  }
+};
+</script>
